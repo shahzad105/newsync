6b97168a8fbcc6a38b65ae74ae94aa7af47ae94a
shahzad821 <shahzadmumtaz821@gmail.com>
2026-03-15 13:07:42 +0500

    fixing logout issues

--- components/HandleLogout.jsx
+++ components/HandleLogout.jsx
@@ -1,33 +1,24 @@
+// components/HandleLogout.tsx
 "use client";
 
 import { signOut } from "next-auth/react";
-import { useRouter } from "next/navigation";
 import { useState } from "react";
-import toast from "react-hot-toast";
 import { FaSignOutAlt } from "react-icons/fa";
 
 export default function LogoutButton() {
-  const router = useRouter();
   const [loading, setLoading] = useState(false);
 
   const handleLogout = async () => {
-    try {
-      setLoading(true);
-      await signOut({ redirect: false });
+    setLoading(true);
 
-      router.refresh();
-    } catch (error) {
-      toast.error(error?.message || "Something went wrong");
-    } finally {
-      setLoading(false);
-    }
+    await signOut({ callbackUrl: "/", redirect: true });
   };
-
   return (
     <button
       onClick={handleLogout}
       disabled={loading}
-      className={`w-full flex items-center gap-2 px-4 py-2.5 rounded-lg font-ui text-[0.82rem] font-medium transition-all border-none cursor-pointer
+      className={`w-full flex items-center gap-2 px-4 py-2.5 rounded-lg font-ui 
+        text-[0.82rem] font-medium transition-all border-none cursor-pointer
         ${
           loading
             ? "text-muted bg-transparent cursor-not-allowed opacity-60"
